888573a797755ec78e9be921c59e2247d5da5225
CharliePayne1 <charliepayne1101@gmail.com>
2020-05-19 19:53:59 +0100

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,10 +3,35 @@ class PostsController < ApplicationController
         @post = Post.find(params[:id])
     end
 
+    def new
+        @post = Post.new
+    end
+
+    def create
+        @post = Post.create(post_params)
+        redirect_to @post
+    end
+
+    def edit
+        @post = Post.find(params[:id])
+    end
+
+    def update
+        @post = Post.find(params[:id])
+        @post.update(post_params)
+        redirect_to @post
+    end
+
     def like 
         @post = Post.find(params[:id])
         @post.likes += 1
         @post.save
-        redirect_to post_path(@post)
+        redirect_to @post
     end
+
+    private 
+
+    def post_params
+        params.require(:post).permit!
+    end 
 end
