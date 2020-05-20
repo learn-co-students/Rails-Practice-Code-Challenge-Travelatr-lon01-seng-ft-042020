1ca7e0d6dbec50536f2ff784a8e07701f4821f86
zbadiru <zbadiru@live.co.uk>
2020-05-20 09:40:22 +0100

    Done.

---
+++ app/controllers/bloggers_controller.rb
@@ -0,0 +1,19 @@
+class BloggersController < ApplicationController
+
+    def new
+        @blogger = Blogger.new
+    end
+
+    def create
+        @blogger = Blogger.create(blogger_params)  
+        redirect_to blogger_path(@blogger.id) 
+    end
+
+    def show
+    @blogger = Blogger.find(params[:id])
+    end
+
+    def blogger_params
+        params.require(:blogger).permit(:name, :age, :bio)
+    end
+end
